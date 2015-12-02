87b8d5ce7a331e286477af6fec4ec34c9a510dd4
sekidaichi <sekidaichi@192.168.11.5>
2015-12-02 21:39:17 +0900

    DBで使用する定数

--- src/dlayer/common/Db_const.java
+++ src/dlayer/common/Db_const.java
@@ -2,4 +2,13 @@ package dlayer.common;
 
 public class Db_const {
 
+	// クラスロード用定数
+	public static final String FOR_NAME = "org.postgresql.Driver";
+	// DB接続先
+	public static final String CONN_ADD = "jdbc:postgresql://localhost:5432/";
+	// DBユーザ名
+	public static final String USER_ID = "test";
+	// DBパスワード
+	public static final String PASSWDORD = "test";
+	
 }
